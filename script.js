0eedaf3e6f40204b44998a14ebd1e295546cfadd
agvan <agvandyke25@gmail.com>
2022-05-15 21:16:45 -0500

    added pseudocode to js

---
+++ script.js
@@ -0,0 +1,9 @@
+/*The application will allow users who are learning JavaScript
+to take a timed quiz on JavaScript that stores high scores, in order 
+for users to compare themselves to their peers.*/
+
+// Users will click the start button and a timer will start.
+// Users wil be presented with a question and another question.
+// When a question is answered incorrectly, time is subtracted from the timer.
+// When all questions are answered OR the timer reaches 0, then the game is over.
+// When the game is over the users can save their initials and score.
