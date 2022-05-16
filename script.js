f0ef8651c1664ee47ab27f4084b037f4a7e5d351
agvan <agvandyke25@gmail.com>
2022-05-16 12:18:44 -0500

    script for the timer

--- script.js
+++ script.js
@@ -10,6 +10,34 @@ for users to compare themselves to their peers.*/
 
 // Users will click the "Start Quiz" button, the time will start and the user will be presented with a question.
 
-function timerStart() {
-    
-}
+//Set the date being counted down to
+var countDownDate = new Date("May 30, 2022 1:10:30").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+    // Get today's date and time
+    var now = new Date().getTime();
+
+    // Find the distance between now and the count down date
+    var distance = countDownDate - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+}, 1000);
+
+// Display the result in the element with the id="timer"
+
+document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "Time is Up!";
+  }
+
+
